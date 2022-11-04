dd143aa2b1bc5db1aabbb86745d47d51dada2f92
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-04 22:20:55 +0800

    test cases for newlines

---
+++ test/spec/newline.ts
@@ -0,0 +1,15 @@
+import {load} from "../../src";
+
+it('should treat LF(0x0A) as newline', () => {
+    const input = '\n';
+    const result = load(input);
+
+    expect(result).toEqual({});
+});
+
+it('should treat CRLF(0x0D 0x0A) as newline', () => {
+    const input = '\r\n';
+    const result = load(input);
+
+    expect(result).toEqual({});
+});
