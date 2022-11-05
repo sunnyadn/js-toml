c95faf29efd29643eb7f2ac71d64cea1ae11023b
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-05 22:46:45 +0800

    add script for visualizing syntax tree

---
+++ script/generateDiagram.ts
@@ -0,0 +1,15 @@
+import {createSyntaxDiagramsCode} from "chevrotain";
+import { writeFileSync } from "fs";
+import {Parser} from "../src/load/parser";
+
+const parser = new Parser();
+
+const outputDiagram = () => {
+  const serializedGrammar = parser.getSerializedGastProductions();
+
+  const htmlText = createSyntaxDiagramsCode(serializedGrammar);
+
+  writeFileSync('syntax-diagram.html', htmlText);
+}
+
+outputDiagram();
