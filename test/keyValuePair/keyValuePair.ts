47e24ead07bce5d40609c02baa5841f4ada14f7f
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-05 22:05:43 +0800

    add test for missing value

--- test/keyValuePair/keyValuePair.ts
+++ test/keyValuePair/keyValuePair.ts
@@ -1,4 +1,4 @@
-import {load} from "../../src";
+import {load, SyntaxParseError} from "../../src";
 
 it('should parse key value pair without whitespace', () => {
   const input = "key=\"value\"";
@@ -12,4 +12,10 @@ it('should parse key value pair with whitespace', () => {
   const result = load(input);
 
   expect(result).toEqual({key: "value"});
+});
+
+it('should throw error if value is missing', () => {
+  const input = "key = # INVALID";
+
+  expect(() => load(input)).toThrow(SyntaxParseError);
 });
