54af4c4a8574ec100425fca5edd9e0e30aff9aa7
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-05 23:12:08 +0800

    add test for bare keys

---
+++ test/key.ts
@@ -0,0 +1,11 @@
+import {load} from "../src";
+
+it('should support bare keys', () => {
+  const input = `key = "value"
+bare_key = "value"
+bare-key = "value"
+1234 = "value"`;
+  const result = load(input);
+
+  expect(result).toEqual({key: "value", bare_key: "value", "bare-key": "value", "1234": "value"});
+});
