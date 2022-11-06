5393b0ebe7101e3cb0353cb3387813601b14a88e
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-06 17:33:10 +0800

    Merge whitespace.ts and newline.ts as a spec.ts

--- test/spec.ts
+++ test/spec.ts
@@ -13,3 +13,17 @@ it('should treat space(0x20) as whitespace', () => {
 
   expect(result).toEqual({});
 });
+
+it('should treat LF(0x0A) as newline', () => {
+  const input = '\n';
+  const result = load(input);
+
+  expect(result).toEqual({});
+});
+
+it('should treat CRLF(0x0D 0x0A) as newline', () => {
+  const input = '\r\n';
+  const result = load(input);
+
+  expect(result).toEqual({});
+});
