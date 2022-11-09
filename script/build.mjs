aded75e06a125785496c4f363ed3e17bd1525a28
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-09 23:27:20 +0800

    turn off minify option for building

--- script/build.mjs
+++ script/build.mjs
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
-import { build } from 'esbuild';
+import {build} from 'esbuild';
 
 build({
   bundle: true,
   entryPoints: ['src/index.ts'],
   external: ['chevrotain', 'xregexp'],
-  minify: true,
   outdir: 'dist',
 }).catch(() => process.exit(1));
