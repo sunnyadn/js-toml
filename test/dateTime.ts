e33179a4472b906b6b6d1c5eeffa70129c23005d
Sunny Yang <guang.yang@thoughtworks.com>
2022-11-10 23:10:03 +0800

    support local date time

--- test/dateTime.ts
+++ test/dateTime.ts
@@ -1,4 +1,5 @@
 import { load } from '../src';
+import timezoneMock = require('timezone-mock');
 
 it('should support offset date-times', () => {
   const input = `odt1 = 1979-05-27T07:32:00Z
@@ -8,9 +9,9 @@ odt3 = 1979-05-27T00:32:00.999999-07:00`;
   const result = load(input);
 
   expect(result).toEqual({
-    odt1: new Date('1979-05-27T07:32:00Z'),
-    odt2: new Date('1979-05-27T00:32:00-07:00'),
-    odt3: new Date('1979-05-27T00:32:00.999999-07:00'),
+    odt1: new Date(Date.UTC(1979, 4, 27, 7, 32, 0)),
+    odt2: new Date(Date.UTC(1979, 4, 27, 7, 32, 0)),
+    odt3: new Date(Date.UTC(1979, 4, 27, 7, 32, 0, 999)),
   });
 });
 
@@ -18,23 +19,27 @@ it('should support replacing T with space in offset date-times', () => {
   const input = 'odt4 = 1979-05-27 07:32:00Z';
   const result = load(input);
 
-  expect(result).toEqual({ odt4: new Date('1979-05-27T07:32:00Z') });
+  expect(result).toEqual({ odt4: new Date(Date.UTC(1979, 4, 27, 7, 32)) });
 });
 
 it('should support millisecond precision in offset date-times', () => {
   const input = 'odt5 = 1979-05-27T07:32:00.999999Z';
   const result = load(input);
 
-  expect(result).toEqual({ odt5: new Date('1979-05-27T07:32:00.999Z') });
+  expect(result).toEqual({
+    odt5: new Date(Date.UTC(1979, 4, 27, 7, 32, 0, 999)),
+  });
 });
 
 it('should support local date-times', () => {
+  timezoneMock.register('US/Pacific');
   const input = `ldt1 = 1979-05-27T07:32:00
-ldt2 = 1979-05-27T00:32:00.999999`;
+ldt2 = 1979-05-27T00:32:00.999`;
   const result = load(input);
 
   expect(result).toEqual({
-    ldt1: new Date('1979-05-27T07:32:00'),
-    ldt2: new Date('1979-05-27T00:32:00.999'),
+    ldt1: new Date(Date.UTC(1979, 4, 27, 14, 32, 0)),
+    ldt2: new Date(Date.UTC(1979, 4, 27, 7, 32, 0, 999)),
   });
+  timezoneMock.unregister();
 });
